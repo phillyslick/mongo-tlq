9e652de196cac937a1e8e9bc5ccdf2c9422f230e
Ben Dixon <ben@hillsbede.co.uk>
2013-06-20 21:11:40 +0100

    adds meta data

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "mongo-tlq"
+maintainer        "Ben Dixon"
+maintainer_email  "ben@hillsbede.co.uk"
+description       "Installs the 10gen mongo server package"
+version           "0.0.1"
+
+recipe "mongo-tlq", "10gen mongo server package"
+
+supports "ubuntu"
