11736b711346d8d23f7f520d48a56b480e707e94
Ben Dixon <ben@hillsbede.co.uk>
2013-12-17 13:13:24 +0000

    allows configuration of a replica set

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "mongo-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@talkingquickly.co.uk"
 description       "Installs the 10gen mongo server package"
-version           "0.0.2"
+version           "0.0.3"
 
 recipe "mongo-tlq", "10gen mongo server package"
 
